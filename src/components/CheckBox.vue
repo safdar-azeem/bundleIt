8651ec7a5fd675b7e3879c8bdde3fb4e43c5c51d
safdar-azeem <safdarazeem00@gmail.com>
2024-11-10 08:52:27 +0500

    Add CheckBox.vue

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+
+defineProps<{
+   isChecked: boolean
+   isIndeterminate: boolean
+}>()
+</script>
+
+<template>
+   <div>
+      <Icon
+         :icon="
+            isIndeterminate
+               ? 'material-symbols:indeterminate-check-box-outline'
+               : isChecked
+               ? 'fluent:checkbox-checked-24-filled'
+               : 'fluent:checkbox-unchecked-16-regular'
+         "
+         :class="`${
+            isChecked || isIndeterminate ? 'text-primary' : 'text-gray-500'
+         } text-fs-4.5`" />
+   </div>
+</template>

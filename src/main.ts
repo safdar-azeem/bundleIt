9cf0e11ef53d961bcda3e0217b4667733de03fab
safdar-azeem <safdarazeem00@gmail.com>
2025-05-02 10:33:47 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
+import { useVersionControl } from './stores/version'
+
+const { checkVersion } = useVersionControl()
+checkVersion()
 
 createApp(App).mount('#app')
